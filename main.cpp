5877c93cdb6980ba01e0099ae7ff00de9d7255e7
Elif <78614794+elffemr@users.noreply.github.com>
2022-01-30 19:30:59 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "employee.h"
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main() {
+	
+	Employee employee;
+	employee.id=12;
+	employee.showInfos();
+	
+	return 0;
+}
